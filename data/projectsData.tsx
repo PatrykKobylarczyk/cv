ededb098abde041bc64b47052969520941559a4d
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-03-20 13:44:46 +0100

    update data

--- data/projectsData.tsx
+++ data/projectsData.tsx
@@ -7,12 +7,73 @@ import {
   SiReact,
   SiSass,
   SiTailwindcss,
+  SiTypescript,
+  SiAxios
 } from "react-icons/si";
 import { BsStripe } from "react-icons/bs";
+import { HiOutlineChartSquareBar } from "react-icons/hi";
+import { AiOutlineForm } from "react-icons/ai";
 
 export const projectsData = [
   {
     id: 1,
+    name: "Dashboard",
+    date: "02/2023-03/2023",
+    titlePL: "Dashboard",
+    titleEN: "Dashboard.",
+    descriptionPL:
+      "Celem projektu było odtworzenie Dashboardu wzorując się na projekcie znalezionym na Behance. Do obsługi wykresów zaimplementowałem bibliotekę charts.js, do logowania Firebase Authentication, do przechowywania danych połączyłem aplikację z Bazą danych Firestore, formularze obsługuje bibliotek React-hook-form.",
+    descriptionEN:
+      "The aim of the project was to recreate the Dashboard based on the project found on Behance. I implemented the charts.js library to handle charts, Firebase Authentication to log in, I connected the application to the Firestore Database to store data, forms are supported by React-hook-form libraries.",
+    stack: [
+      { name: "Next", icon: <SiNextdotjs /> },
+      { name: "Typescript", icon: <SiTypescript /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+      { name: "Tailwind CSS", icon: <SiTailwindcss /> },
+      { name: "Charts JS", icon: <HiOutlineChartSquareBar /> },
+      { name: "React hook form", icon: <AiOutlineForm /> },
+    ],
+    links: [
+      {
+        name: "github",
+        link: "https://github.com/PatrykKobylarczyk/duo-milonga",
+      },
+      {
+        name: "live",
+        link: "https://duomilonga.pl/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Fishky",
+    date: "03/2023-03/2023",
+    titlePL: "Fishky.",
+    titleEN: "Fishky.",
+    descriptionPL:
+      "Aplikacja z popularnymi fiszkami do nauki języków lub cokolwiek sobie wymarzysz.",
+    descriptionEN:
+      "An app with popular flashcards for learning languages or whatever category you can think of. ",
+    stack: [
+      { name: "Next", icon: <SiNextdotjs /> },
+      { name: "Typescript", icon: <SiTypescript /> },
+      { name: "Firebase", icon: <SiFirebase /> },
+      { name: "React hook form", icon: <AiOutlineForm /> },
+      { name: "Sass", icon: <SiSass /> },
+    ],
+    links: [
+      {
+        name: "github",
+        link: "https://github.com/PatrykKobylarczyk/fishky-v2",
+      },
+      {
+        name: "live",
+        link: "https://fishky-v2.vercel.app",
+      },
+    ],
+  },
+  {
+    id: 3,
     name: "Duo Milonga",
     date: "12/2022-01/2023",
     titlePL: "Strona internetowa zespołu muzycznego.",
@@ -38,13 +99,13 @@ export const projectsData = [
     ],
   },
   {
-    id: 2,
+    id: 4,
     name: "Netflix clone",
     date: "11/2022-12/2022",
     titlePL: "Klon Platformy Netflix.",
     titleEN: "Netflix clone.",
     descriptionPL:
-      "Głównym celem było połączenie aplikacji z FIrebase, podłączenie płatności, odwzorowanie oryginalnej strony.",
+      "Głównym celem było połączenie aplikacji z Firebase, podłączenie płatności, odwzorowanie oryginalnej strony.",
     descriptionEN:
       "The main goals were connection to Firebase, possibility of payment, mapping the original.",
     stack: [
@@ -66,28 +127,31 @@ export const projectsData = [
     ],
   },
   {
-    id: 3,
-    name: "Fishky",
-    date: "05/2022-06/2022",
-    titlePL: "Fishky.",
-    titleEN: "Fishky.",
+    id: 5,
+    name: "Weather App",
+    date: "06/2022-07/2022",
+    titlePL: "Aplikacja pogodowa.",
+    titleEN: "Weather App.",
     descriptionPL:
-      "Aplikacja z popularnymi fiszkami do nauki języków lub cokolwiek sobie wymarzysz.",
+      "Aplikacja pobiera dane pogodowe poprzez API z serwisu openweathermap.org, jest całkowicie responsywna.",
     descriptionEN:
-      "An app with popular flashcards for learning languages or whatever category you can think of. ",
+      "The application downloads weather data via API from the openweathermap.org service, it is completely responsive.",
     stack: [
       { name: "React", icon: <SiReact /> },
       { name: "Sass", icon: <SiSass /> },
+      { name: "Axios", icon: <SiAxios /> },
+
     ],
     links: [
       {
         name: "github",
-        link: "https://github.com/PatrykKobylarczyk/fishky",
+        link: "https://github.com/PatrykKobylarczyk/Netflix",
       },
       {
         name: "live",
-        link: "https://patrykkobylarczyk.github.io/fishky/",
+        link: "https://netflix-peach-ten.vercel.app/",
       },
     ],
   },
+  
 ];
